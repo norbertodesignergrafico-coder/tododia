c1d6ee3074f0ba70addc6a39be2f389b2cf8cb31
norbertodesignergrafico-coder <norberto.designergrafico@gmail.com>
2026-01-13 16:05:12 -0300

    Add files via upload

--- App.tsx
+++ App.tsx
@@ -1,4 +1,3 @@
-
 import React, { useState, useEffect, useRef } from 'react';
 import { AuditData, SmartGoal, VisionBoardItem, Habit, AppView } from './types';
 import { Welcome } from './views/Welcome';
